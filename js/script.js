279a577b09e715ac3c9c8f196fdec85c483e6759
MadritB <t119004133@gmail.com>
2022-12-26 11:28:25 +0530

    Implement getComputerChoice() fucntion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("Ready to go!!!");
+
+function getComputerChoice()
+{
+    // Generates a random number from 0-2
+    let randomNumber = (Math.floor(Math.random() * 100) % 3);
+    console.log(randomNumber);
+
+    // Based on random number, return a choice
+    switch(randomNumber)
+    {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissor";
+    }
+}
